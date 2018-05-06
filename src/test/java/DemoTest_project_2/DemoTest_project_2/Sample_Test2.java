c73c04cb800a2657b1054978135ba48c4d004da1
imran0801 <imu.imran123@gmail.com>
2018-05-06 10:35:22 +0530

    my second code to commit to git

---
+++ src/test/java/DemoTest_project_2/DemoTest_project_2/Sample_Test2.java
@@ -0,0 +1,10 @@
+package DemoTest_project_2.DemoTest_project_2;
+
+public class Sample_Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("My Second program to commit to git");
+
+	}
+
+}
