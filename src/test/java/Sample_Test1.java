c36d595edaafebcb9a1906f53e5ec765cf1d79e6
imran0801 <imu.imran123@gmail.com>
2018-06-10 17:22:22 +0530

    updated the settings

---
+++ src/test/java/Sample_Test1.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+
+public class Sample_Test1 {
+	@Test()
+	public void sample_M()
+	{
+		System.out.println("Hi testng method");
+	}
+
+}
---
+++ src/test/java/com/test/practise/Sample_Test2.java
@@ -0,0 +1,12 @@
+package com.test.practise;
+
+import org.testng.annotations.Test;
+
+public class Sample_Test2 {
+
+	@Test()
+	public void sample_M()
+	{
+		System.out.println("Hi testng method");
+	}
+}
